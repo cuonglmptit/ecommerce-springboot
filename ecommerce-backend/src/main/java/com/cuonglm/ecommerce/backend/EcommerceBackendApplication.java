383ebe3dfb2bbfd98c6ea7d07896e9b865cbbb24
cuonglmptit <131643613+cuonglmptit@users.noreply.github.com>
2025-07-24 21:12:55 +0700

    chore(config): configure actuator with application.properties and build.gradle
    - Enable Spring Boot Actuator endpoints
    - Add actuator dependency to build.gradle

--- ecommerce-backend/src/main/java/com/cuonglm/ecommerce/backend/EcommerceBackendApplication.java
+++ ecommerce-backend/src/main/java/com/cuonglm/ecommerce/backend/EcommerceBackendApplication.java
@@ -1,13 +1,15 @@
 package com.cuonglm.ecommerce.backend;
 
+import jakarta.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackendApplication.class, args);
+    }
 
 }
